f3c8d5ea983d81792fefb916e96fd6caec079a96
Michael Prechter <m.r.prechter@gmail.com>
2017-07-05 20:34:31 -0400

    Authentication appears to be solid

--- app.js
+++ app.js
@@ -7,24 +7,26 @@ const mustacheExpress = require('mustache-express');
 const models = require('./models')
 const app = express();
 const userRouter = require('./routes/users');
+const parseurl = require('parseurl'); //do i need this?
 
 app.engine('mustache', mustacheExpress());
+
 app.set('views', './views');
 app.set('view engine', 'mustache');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(expressValidator());
-
 app.set('trust proxy', 1) // trust first proxy
-app.use(session({ //have not installed sessions yet - 7/2/17 7:09pm
-  secret: 'keyboard cat',
+
+app.use(session({
+  secret: 'rebusakasafram',
   resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
+  saveUninitialized: true
 }));
 
+app.use(express.static('public'));
+app.use(expressValidator());
 app.use(userRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,81 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
-const models = require('../models')
+const models = require('../models');
+const parseurl = require('parseurl');//do i need this??
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 
-
-router.get('/', function(req, res) {
-  res.render('index')
-});
-
+//the sign-up route
 router.get('/signup', function(req, res) {
   res.render('signup')
 });
 
 router.post('/signup-route', function(req, res) {
-    const user = models.users.build({
-      username: req.body.usernameEntry,
-      password: req.body.passwordEntry,
-      name: req.body.nameEntry
-    })
+  const user = models.users.build({
+    username: req.body.usernameEntry,
+    password: req.body.passwordEntry,
+    name: req.body.nameEntry
+  });
   user.save();
   res.redirect('/')
-})
+});
 
 
+
+//the login route
+router.get('/login', function(req, res) {
+  res.render('login')
+});
+
+router.post('/login-route', function(req, res) {
+  let username = req.body.usernameLogin;
+  let password = req.body.passwordLogin;
+  models.users.findOne({
+    where: {
+      username: username,
+      password: password
+    }
+  }).then(function(user) {
+    if (user == null) {
+      res.redirect('/login')
+    }
+    req.session.user = user
+    res.redirect('/')
+  })
+});
+
+
+
+//the homepage route
+router.get('/', function(req, res) {
+  if(!req.session.user) {
+  //   return res.status(401).send("You're not logged in yet :/");
+  // }
+  res.render('index', {
+    userSession: req.session
+  })
+});
+
+
+
+//the post route
+router.get('/post', function(req, res) {
+  res.render('post')
+});
+
+router.post('/post-route', function(req, res){
+
+});
+
+
+
+//the logout route
+router.get('/logout', function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+});
 
 module.exports = router;
